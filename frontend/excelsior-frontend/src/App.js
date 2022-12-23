1d3f133e667b052bd4634202f7020e58968e6a58
Adarsh Rao <raoadarsh123@gmail.com>
2022-12-22 20:54:23 -0500

    fe

--- frontend/excelsior-frontend/src/App.js
+++ frontend/excelsior-frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import MenuBar from './MenuBar.js'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import contract from './contracts/Auction.json';
 import { ethers } from 'ethers';
 
@@ -25,6 +25,11 @@ function App(props) {
     var temp = 0;
     const [currentAccount, setCurrentAccount] = useState(null);
     const [owner, setOwner] = useState(null);
+    const [auctionInfo, showAuctionInfo] = useState(false);
+    const [highestBidder, setHighestBidder] = useState(null);
+    const [highestBid, setHighestBid] = useState(null);
+    const [auctionEnd, setAuctionEnd] = useState(null);
+
     const checkWalletIsConnected = () => {
       const { ethereum } = window;
       if (!ethereum) {
@@ -73,17 +78,73 @@ function App(props) {
           const provider = new ethers.providers.Web3Provider(ethereum);
           const signer = provider.getSigner();
           const contract = new ethers.Contract(contractAddress, contractABI, signer)
-          if (currentAccount === contract.seller()) {
+          console.log("called");
+          const bid = await contract.highestBid();
+          const topBidder = await contract.highestBidder();
+          const auctionEndTime = await contract.endAt();
+          setHighestBid(ethers.utils.formatEther(bid));
+          setHighestBidder(ethers.utils.getAddress(topBidder));
+          setAuctionEnd(ethers.utils.formatUnits(auctionEndTime, 0));
+          const seller = await contract.seller();
+          console.log(seller);
+          console.log(currentAccount);
+          console.log(ethers.utils.getAddress(seller) == ethers.utils.getAddress(currentAccount));
+          if (ethers.utils.getAddress(seller) == ethers.utils.getAddress(currentAccount)) {
             console.log("You are the owner!")
             setOwner(true);
           }
           else {
             setOwner(false);
           }
+          showAuctionInfo(true);
         }
       }
       catch (error) {
         console.log(error);
+      }
+    }
+
+    // Button to start the auction
+    const startAuctionHandler = async () => {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer)
+        // Get the nft address, nft id, and starting price
+        const nftAddress = ethers.utils.getAddress(document.querySelector("#nftAddress").value);
+        const nftId = document.querySelector("#nftId").value;
+        const startingPrice = parseInt(document.querySelector("#startingPrice").value);
+        const startTxn = await contract.start(nftAddress, nftId, startingPrice);
+        await startTxn.wait();
+        console.log("Auction Started!");
+      }
+    }
+
+
+    const endAuctionHandler = async () => {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer)
+        const endTxn = await contract.end();
+        await endTxn.wait();
+        console.log("Auction Ended!");
+      }
+    }
+
+    const bidHandler = async () => {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer)
+        const bidValue = document.querySelector("#bidValue").value;
+        const bidValueWei = ethers.utils.parseEther(bidValue);
+        const bidTxn = await contract.bid({value: bidValueWei});
+        await bidTxn.wait();
+        alert("Bid Complete!");
       }
     }
 
@@ -101,38 +162,86 @@ function App(props) {
         </button>
       )
     }
+
+    // Button that allows user to start NFT auction and accepts nft address, nft id, and starting bid value
+    const startAuctionButton = () => {
+      return (
+        <div>
+          <input type="text" placeholder="Enter contract address" id="nftAddress" />
+          <input type="text" placeholder="Enter NFT ID" id="nftId" />
+          <input type="text" placeholder="Enter starting bid value" id="startingPrice" />
+          <button onClick={startAuctionHandler} className='cta-button auction-start-button' style={{backgroundColor: "green"}}>
+            Start Auction
+          </button>
+        </div>
+      )
+    }
+
+    
+    const endAuctionButton = () => {
+      return (
+        <button onClick={endAuctionHandler} className='cta-button auction-end-button' style={{backgroundColor: "red"}}>
+          End Auction
+        </button> 
+      )
+    }
+
+
+    // Button that allows user to bid on an NFT by entering a value
+    const bidButton = () => {
+      const date = new Date(auctionEnd * 1000);
+      return (
+        <div>
+          <input type="text" placeholder="Enter Bid Value" id="bidValue" />
+          <button onClick={bidHandler} className='cta-button bid-button' style={{backgroundColor: "green"}}>
+            Bid
+          </button>
+          <div>
+            <h3>Auction Info</h3>
+            <p>Highest Bid: {highestBid}</p>
+            <p>Highest Bidder: {highestBidder}</p>
+            <p>End Time: {date.toLocaleString()}</p>
+          </div>
+        </div>
+      )
+    }
+
     useEffect(() => {
       checkWalletIsConnected();
-    }, [])
+    }, []);
   
     return (
       <div>
         <MenuBar />
-            <div style={{ textAlign: 'center', marginTop: '5vh' }}>
-              <h3>Welcome to Excelsior Climate!</h3>
-            </div>
-            <div style={{ textAlign: 'center', marginTop: '5vh', marginLeft: '40vh', marginRight: '40vh'}}>
-              <h5>We aim to combat climate change through awareness and fundraising via NFTs that symbolize global warming through 1-of-1 special-edition-release NFTs everytime the temperature raises 0.0005 degrees Celsius. </h5>
-            </div>
-            <ImageGallery />
+          <div style={{ textAlign: 'center', marginTop: '5vh' }}>
+            <h3>Welcome to Excelsior Climate!</h3>
+          </div>
+          <div style={{ textAlign: 'center', marginTop: '5vh', marginLeft: '40vh', marginRight: '40vh'}}>
+            <h5>We aim to combat climate change through awareness and fundraising via NFTs that symbolize global warming through 1-of-1 special-edition-release NFTs everytime the temperature raises 0.0005 degrees Celsius. </h5>
+          </div>
+          <ImageGallery />
 
-            <div style={{textAlign: 'center'}} dangerouslySetInnerHTML={{ __html: "<iframe title='World average temperature (°C)' src='https://www.theworldcounts.com/embeds/counters/21?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20' style='border: none' height='100' width='300'></iframe>"}} />
+          <div style={{textAlign: 'center'}} dangerouslySetInnerHTML={{ __html: "<iframe title='World average temperature (°C)' src='https://www.theworldcounts.com/embeds/counters/21?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20' style='border: none' height='100' width='300'></iframe>"}} />
+          {!auctionInfo ?
             <div style={{ textAlign: 'center'}}>
               {currentAccount ? 
-              
-              
               <div style={{marginLeft: '25em', marginRight: '25em', marginTop:'3em', padding: '1em', border: '1px solid #ccc',borderRadius: '5px',backgroundColor: '#f2f2f2'}}>
                 Your wallet <strong>{currentAccount}</strong> is connected.
                 {enterAuctionButton()}
               </div>
-             
-              
-              
-              
-              
               : connectWalletButton()}
-            </div>  
-
+            </div>
+            :
+            <div style={{ textAlign: 'center'}}>
+              {owner ? <div style={{marginLeft: '25em', marginRight: '25em', marginTop:'3em', padding: '1em', border: '1px solid #ccc',borderRadius: '5px',backgroundColor: '#f2f2f2'}}>
+                {startAuctionButton()}
+                {endAuctionButton()}
+              </div> : <div style={{marginLeft: '25em', marginRight: '25em', marginTop:'3em', padding: '1em', border: '1px solid #ccc',borderRadius: '5px',backgroundColor: '#f2f2f2'}}>
+                {bidButton()}
+                </div>
+              }
+            </div>
+          }
       </div>
     );
 }
